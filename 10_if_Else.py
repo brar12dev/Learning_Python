3fbc2ecf3055cc378cb890e92dae7dd0f607c84c
Brar <brarraj0009@gmail.com>
2020-11-25 07:14:29 -0800

    first commit

--- 10_if_Else.py
+++ 10_if_Else.py
@@ -6,11 +6,26 @@ var3 = int(input("Give a number \n"))
 if var3 == var2 or var3 == var1:
     print("equal")
 
-elif var3>var2 or var3 > var1:
+elif var3 > var2 or var3 > var1:
     print("greater")
     
 
-elif (var3<var2) or (var3 < var1):
+elif (var3 < var2) or (var3 < var1):
     print("smaller")
 
-""" YOU CAN USE () OR CAN DIRECTLY GIVE CONDITIONS ARFTER USING IT AFTER WRITING if CONDITION """
+""" NOTE : YOU CAN USE () OR CAN DIRECTLY GIVE CONDITIONS ARFTER USING IT AFTER WRITING if CONDITION """
+
+list1 = [12,23,34,45,1,2,3,4]
+
+if(var3 in list1):
+    print("yes ", var3 , " is present")
+else:
+    print("no ", var3 , " is not present")
+
+
+if (var3 in list1) and (12 in list1) :
+    print("yes ", var3 , " is present in list 1")
+elif var3 in list1 or 12 in list1:
+    print("yes ", var3 , " is present in list 1")
+else :
+    print("not ", var3 , " is not present")
---
+++ 11_short_hand_If_else.py
@@ -0,0 +1,11 @@
+""" the short hand for writing if else statement is 
+ > SYNTAX is :  
+                CODE  if(condition) else Code
+                or
+                CODE  if condtion else CODE
+"""
+# simple example ------ 
+
+print("1==2") if 1==2 else print("1 != 2")
+#  example 2
+print("12 - 12 = 12") if(12-12 == 12) else print("12 - 12 == 0")
