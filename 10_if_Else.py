df8f86f395cc379fc142adcc9b6b2abaaa4ccd72
Brar <brarraj0009@gmail.com>
2020-11-25 07:02:30 -0800

    first commit

---
+++ 10_if_Else.py
@@ -0,0 +1,16 @@
+var1 = 6
+var2 = 56
+
+var3 = int(input("Give a number \n"))
+
+if var3 == var2 or var3 == var1:
+    print("equal")
+
+elif var3>var2 or var3 > var1:
+    print("greater")
+    
+
+elif (var3<var2) or (var3 < var1):
+    print("smaller")
+
+""" YOU CAN USE () OR CAN DIRECTLY GIVE CONDITIONS ARFTER USING IT AFTER WRITING if CONDITION """
